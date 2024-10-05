ab58d82cc0cec8ad09726d95c0e3944066390183
unknown <qmutawali@gmail.com>
2024-10-05 20:56:18 +0700

    add: deploy workflows

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Mutawali-s-Portfolio/"
+  base: "/Mutawali-s-Portfolio/",  // Tambahkan koma di sini
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
